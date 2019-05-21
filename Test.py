612cb1a77c25c585d141f483b78c6cbc9cf50d4c
Syzygianinfern0 <spsharan2000@gmail.com>
2019-05-21 16:48:29 +0530

    Stacking Replay Implemented

--- Test.py
+++ Test.py
@@ -1,8 +1,11 @@
 import gym
+import cv2
 
 env = gym.make('SpaceInvaders-v0')
 env.reset()
 for _ in range(3000):
-    env.step(env.action_space.sample())
+    observation, reward, done, info = env.step(env.action_space.sample())
+    cv2.imshow('obs', observation)
     env.render('human')
 env.close()
+cv2.destroyAllWindows()
--- preprocessframe.py
+++ preprocessframe.py
@@ -1,9 +1,28 @@
-import cv2
+from skimage import transform
+from skimage.color import rgb2gray
+from skimage import io
 
 
+# def pre_process(frame):
+#     gray_frame = rgb2gray(frame)
+#     # cropped_frame = gray_frame[8:-12, 4:-12]
+#     # normalised_frame = cropped_frame / 255.0
+#     # perfect_frame = transform.resize(normalised_frame, [110, 84])
+#     return gray_frame
 def pre_process(frame):
-    gray_frame = cv2.imread(frame, 0)
-    cropped_frame = gray_frame[8:-12, 4:-12]
-    normalised_frame = cropped_frame / 255.0
-    perfect_frame = cv2.resize(normalised_frame, [110, 84])
-    return perfect_frame
+    # Greyscale frame
+    gray = rgb2gray(frame)
+
+    # Crop the screen (remove the part below the player)
+    # [Up: Down, Left: right]
+    cropped_frame = gray[8:-12, 4:-12]
+
+    # Normalize Pixel Values
+    normalized_frame = cropped_frame / 255.0
+
+    # Resize
+    # Thanks to Mikołaj Walkowiak
+    preprocessed_frame = transform.resize(normalized_frame, [110, 84])
+
+    io.imshow(preprocessed_frame)
+    io.show()
